6d93d9bc04144bd8c54e7fc43d068947a539c6ee
edwinv <edwwin527@gmail.com>
2019-06-10 13:39:53 -0500

    se agrega para tener variables de entorno en un archivo aparte

--- data_base/connection.js
+++ data_base/connection.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql');
 const logger = require('../services/logger');
+require('dotenv').config();
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
 
 let db = async ()=>{
     try{
         return mysql.createConnection({
-            host:'localhost',
-            user:'root',
-            password: 'root',
-            database:'world_project_db'
+            host: DB_HOST,
+            user: DB_USER,
+            password: DB_PASSWORD,
+            database: DB_NAME
         });
     }catch(err){
         logger.error(' Problem to connect to the database: ', err);
